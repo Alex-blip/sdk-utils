7f315574e7272fdbbee35b4fc4805676ecf237fc
“e <18790334713@163.com>
2020-12-30 11:55:07 +0800

    修改阿里云oss上传

--- oss/aliyun/upload_oss_file.py
+++ oss/aliyun/upload_oss_file.py
@@ -1,26 +1,29 @@
 # -*- coding: UTF-8 -*-
 __author__ = "lmy"
 
-import time
 import oss2
+import os
 
-yourAccessKeyId = "yourAccessKeyId"  # 通过账号获得accessKey
-yourAccessKeySecret = "yourAccessKeySecret"  # 通过账号获得secretAccessKey
-yourBucketName = "yourBucketName"  # 你的bucket名
-yourObjectName = "yourObjectName"
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 
-def upload_oss_file(key):
-    endpoint = 'http://oss-cn-shanghai.aliyuncs.com'
+yourAccessKeyId = os.environ.get('yourAccessKeyId')  # 通过账号获得accessKey
+yourAccessKeySecret = os.environ.get('yourAccessKeySecret')  # 通过账号获得secretAccessKey
+yourBucketName = "y-home"  # 你的bucket名
+endpoint = 'oss-cn-beijing.aliyuncs.com'
 
+
+def uploadFileBytes(fileName, fileContent):
+    """
+    :param fileName: 文件名称
+    :param fileContent: 文件二进制内容
+    :return:
+    """
+    Folder = "upload"
     auth = oss2.Auth(yourAccessKeyId, yourAccessKeySecret)
-    bucket = oss2.Bucket(auth, endpoint, yourBucketName)
-    current_fold = time.strftime('%Y-%m-%d', time.localtime())
-    current_file_path = yourObjectName + key
-    file_path = '你的项目路径' + key
-    # 上传
-    bucket.put_object_from_file(current_file_path, file_path)
+    bucket = oss2.Bucket(auth, endpoint, yourBucketName, connect_timeout=30)
+    result = bucket.put_object(Folder + '/' + fileName, fileContent)
+    print(result)
 
 
-key = '文件名'
-upload_oss_file(key)
